00dc62d9a727cc36e0b9aaaba3abe1b64ed9a8f0
Sota Suzuki <sotasuzuki@umee.be>
2017-10-20 15:54:54 +0900

    first commit

---
+++ try-vuexcounter/webpack.config.babel.js
@@ -0,0 +1,10 @@
+import webpack from 'webpack'
+import path from 'path'
+
+export default {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath:
+  }
+}
